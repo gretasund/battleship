4c8d25246c0ef563742f376b7f8e48842d43a56f
Greta <greta.sund@gmail.com>
2022-04-19 15:56:22 +0100

    File is read into a string, no more than 15 chars / digits, values between 0 and 100, name only English alphabet

--- game.c
+++ game.c
@@ -13,7 +13,7 @@ int print_menue(void) {
     printf("5) Execute an airstrike\n");
     printf("6) Quit\n");
 
-    while(status==0){
+    while(status == 0){
         
         // get user input
         printf("Please enter a number: ");
@@ -48,11 +48,61 @@ int print_menue(void) {
 
 
 
-int main(void) {
+void load_target_file(void) {
+    
+    // declare variables
+    char file_name[30] = {};
+    FILE *fp;
+    // char buffer[255];
+    // char buffer2[255];
+    
+    
+    // get user input
+    printf("Enter a target file: \n");
+    scanf("%s", file_name);
+    
+    // open the file
+    fp = fopen("file_valid.txt","r");
+    
+    // display error message if invalid
+    if (fp == NULL) {
+    printf("Invalid file.\n");
+    }
+    
+    // read and print the whole file
+    fseek(fp, 0, SEEK_END);                 // sets the offset to the end
+    long fpsize = ftell(fp);                // returns the given file position
+    rewind(fp);                             // sets the position to the beginning
 
+    char *string = malloc(fpsize + 1);      // check the return value of malloc & fread!
+    fread(string, 1, fpsize, fp);           // read the whole file
+    fclose(fp);                             // close the file
+
+    string[fpsize] = 0;                     // free string
+    printf("%s\n", string);
+}
+
+
+
+int main(void) {
+    
+    // declare variables
     int option;
     
+    // user chooses an option
     option = print_menue();
     printf("Your choice is %d\n", option);
+        
+    
+    // perform the correct option
+    switch (option) {
+        case 1:
+            load_target_file();
+            break;
+            
+        default:
+            printf("Not implemented yet.\n");
+            break;
+    }
 
 }
